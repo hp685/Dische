f071e000af99e74f7c4d3e7ed39a0d0da67c2363
hp685 <harsh@nyu.edu>
2018-04-05 05:30:44 -0700

    threaded echo server

--- server.c
+++ server.c
@@ -2,10 +2,50 @@
 // Created by harshpatel on 3/30/18.
 //
 
+#include<arpa/inet.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+#define NUM_THREADS 10
+#define MAX_BYTES 65536
+
+
 
 const int LISTEN_BACKLOG = 512;
+
+
+void handle_message(int fd){
+    char* buffer;
+    ssize_t bytes_read = read(fd, buffer, MAX_BYTES);
+
+    if (bytes_read <= 0){
+      perror("Error reading from socket");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("Bytes read: {%lu} with data {%s}", bytes_read, buffer);
+
+    write(fd, buffer, bytes_read);
+    close(fd);
+}
+
+
+void handle_in_thread(int fd){
+  pthread_t  thread;
+  void *res;
+  int rc = pthread_create(thread, NULL, handle_message, (void*) fd);
+  if (rc != 0){
+    perror("Error creating thread");
+    exit(EXIT_FAILURE);
+  }
+  pthread_join(thread, &res);
+}
+
 
 void listen_forever(){
 
@@ -23,12 +63,12 @@ void listen_forever(){
                 perror("Error on listen");
             }
             while(1){
-                client_fd = accept(sock, (struct sockaddr *) &client_address, sizeof(client_address));
-
+                int client_fd = accept(sock, (struct sockaddr *) &client_address, sizeof(client_address));
+		            handle_in_thread(client_fd);
             }
         }
         else{
-            perror("Cannot bind socket %d", sock);
+            perror("Cannot bind socket");
         }
 
     }
@@ -38,7 +78,6 @@ void listen_forever(){
     }
 
 int main(){
-
     listen_forever();
     return 0;
 }
