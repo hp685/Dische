0360895ce5c441e86c861b186608c92057512a3e
Harsh Patel <harsh@nyu.edu>
2018-04-07 15:40:30 -0700

    update

--- threadpool.c
+++ threadpool.c
@@ -5,17 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "queue.h"
-#define NUM_THREADS 1
-#define POLL_INTERVAL 1
-
-typedef struct {
-
-  pthread_mutex_t lock;
-  pthread_t threads[NUM_THREADS];
-  Queue* work_q;
-
-}ThreadPool;
-
+#include "threadpool.h"
 
 
 
