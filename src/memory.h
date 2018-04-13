47fe695d98e843f2a26661e6624e8643f429bf40
hp685 <harsh@nyu.edu>
2018-04-13 05:02:17 -0700

    init functions

---
+++ src/memory.h
@@ -0,0 +1,14 @@
+#include <malloc.h>
+
+
+void* fmalloc(size_t size){
+
+    /*check free lists if available*/
+}
+
+void* ffree(void *ptr){
+  /* Return block back to the pool*/
+
+  /* If # blocks in pool above a threshold, release that back to the os*/
+  
+}
